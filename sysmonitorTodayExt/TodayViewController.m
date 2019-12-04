ea56e718eb47152c45f67efc8261d91cbea21f77
LeviHan <levihan@isystantapp.com>
2019-12-05 01:16:24 +0800

    today extension UI

--- sysmonitorTodayExt/TodayViewController.m
+++ sysmonitorTodayExt/TodayViewController.m
@@ -12,8 +12,15 @@
 
 @interface TodayViewController () <NCWidgetProviding>
 
-@property (weak, nonatomic) IBOutlet UILabel *deviceModelLabel;
+@property (weak, nonatomic) IBOutlet UILabel *freeRAMLabel;
+@property (weak, nonatomic) IBOutlet UILabel *usedRAMLabel;
+@property (weak, nonatomic) IBOutlet UILabel *upNetSpeedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *downNetSpeedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *freeDiskLabel;
+@property (weak, nonatomic) IBOutlet UILabel *UsedDiskLabel;
 
+@property (nonatomic, strong) NSTimer *timerForMemory;
+@property (nonatomic, strong) NSTimer *timerForSpeed;
 @end
 
 @implementation TodayViewController
@@ -22,13 +29,69 @@
     [super viewDidLoad];
     [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
     [[HLSystemMonitor sharedMonitor] parseDeviceFile];
-    
-    NSString *deviceModel   = [HLDeviceInformation getDeviceName];
-    self.deviceModelLabel.text = deviceModel;
-    
-    
-    NSUserDefaults *group = [[NSUserDefaults alloc] initWithSuiteName:@"group.com.hl1987.app.isystant-lite"];
+    [HLSystemMonitor startMonitorNetworkFlowWith:[HLSystemMonitor sharedMonitor].netowrkRefreshInterval];
+    [HLSystemMonitor startMemoryMonitor:[HLSystemMonitor sharedMonitor].memoryRefreshInterval];
+
+//    NSUserDefaults *group = [[NSUserDefaults alloc] initWithSuiteName:@"group.com.hl1987.app.isystant-lite"];
 }
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    _timerForSpeed = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(refreshNetworkSpeed) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:_timerForSpeed forMode:NSRunLoopCommonModes];
+    [_timerForSpeed fire];
+    
+    _timerForMemory = [NSTimer scheduledTimerWithTimeInterval:1.67 target:self selector:@selector(refresh) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:_timerForMemory forMode:NSRunLoopCommonModes];
+    [_timerForMemory fire];
+    
+    [self configDiskInfo];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [_timerForSpeed invalidate];
+    _timerForSpeed = nil;
+    [_timerForMemory invalidate];
+    _timerForMemory = nil;
+    [HLSystemMonitor stopMonitor];
+}
+
+- (void)refresh {
+    [self configMemory];
+}
+
+- (void)refreshNetworkSpeed {
+
+    NSString *downloadSpeed = [HLNetworkMonitor network].downloadNetworkSpeed;
+    NSString *uploadSpeed = [HLNetworkMonitor network].uploadNetworkSpeed;
+    //通知主线程刷新
+    dispatch_async(dispatch_get_main_queue(), ^{
+            self.upNetSpeedLabel.text = uploadSpeed;
+            self.downNetSpeedLabel.text = downloadSpeed;
+    });
+
+}
+
+- (void)configMemory {
+    NSString *usedmemory = [HLMemoryMonitor monitor].totalUsedMemory ;
+    NSString *freememory = [HLMemoryMonitor monitor].freeMemory ;
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.freeRAMLabel.text = freememory;
+        self.usedRAMLabel.text = usedmemory;
+    });
+}
+
+/// 磁盘存储信息
+- (void)configDiskInfo {
+    NSDictionary *dic = [HLDeviceInformation getDeviceSize];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.freeDiskLabel.text = dic[@"freeSpace"];
+        self.UsedDiskLabel.text = dic[@"usedSpace"];
+    });
+
+}
+
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
     // Perform any setup necessary in order to update the view.
